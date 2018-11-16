09741f3bbe7a891176c11b7474814dea6c951d27
Kseniaveh <31632695+Kseniaveh@users.noreply.github.com>
2018-11-16 16:59:37 +0300

    Update RegistrationBook.py

--- SR_var/1.2/RegistrationBook.py
+++ SR_var/1.2/RegistrationBook.py
@@ -14,15 +14,15 @@ class Registration:
     так как email - это уникаьное значение"""
     def DeleteMember(self, name):
         for participant in self.participants:
-            if participant.get("participant email") == email:
-                self.participants.remove(participant)
+            if participant.get("participant email") == email: # ищет совпадение с существующим email-ом 
+                self.participants.remove(participant) 
 
     """Создаем метод - Запись в файл"""
     def RecordFile(self):
         import json # чтобы преобразовать в json из dict в котором list
         with open("./new_file.json", 'a') as file:
-            json_data = { "all_participant": self.participants } 
-            # dumps = сделай мне пожалуйста из dict json
+            json_data = { "all_participant": self.participants } # это делает так { [{},{}] }, делает  валидный json 
+            # dumps = сделай мне из dict json
             # indent = количество пробелов в отступах
             file.write(json.dumps(json_data, indent=4))
             
